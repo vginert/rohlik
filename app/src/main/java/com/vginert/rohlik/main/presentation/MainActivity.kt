ce8f174f23b44bbf80d53e37a3ba28e5ad47ac16
Vicente Giner <giner@billiger-mietwagen.de>
2022-06-05 20:15:58 +0200

    Add surface to main activity to enable compose background

--- app/src/main/java/com/vginert/rohlik/main/presentation/MainActivity.kt
+++ app/src/main/java/com/vginert/rohlik/main/presentation/MainActivity.kt
@@ -3,9 +3,10 @@ package com.vginert.rohlik.main.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
 import com.vginert.rohlik.shared.presentation.theme.RohlikTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +14,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             RohlikTheme {
-                MainRoute()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MainRoute()
+                }
             }
         }
     }
