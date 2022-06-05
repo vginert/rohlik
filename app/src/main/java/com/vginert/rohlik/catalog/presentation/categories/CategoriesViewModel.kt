6fcce80348503f25ee19a602a314282542f98336
Vicente Giner <giner@billiger-mietwagen.de>
2022-06-05 20:36:27 +0200

    Remove execute use case implementation from view model

--- app/src/main/java/com/vginert/rohlik/catalog/presentation/categories/CategoriesViewModel.kt
+++ app/src/main/java/com/vginert/rohlik/catalog/presentation/categories/CategoriesViewModel.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.viewModelScope
 import com.vginert.rohlik.catalog.domain.Category
 import com.vginert.rohlik.catalog.domain.use_cases.GetCategoriesUseCase
 import com.vginert.rohlik.catalog.presentation.categories.models.asPresentation
-import com.vginert.rohlik.shared.presentation.lifecycle.executeUseCase
+import com.vginert.rohlik.shared.core.coroutines.executeUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
--- app/src/main/java/com/vginert/rohlik/shared/presentation/lifecycle/ViewModelExtensions.kt
+++
@@ -1,9 +0,0 @@
-package com.vginert.rohlik.shared.presentation.lifecycle
-
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.vginert.rohlik.shared.core.coroutines.executeUseCase
-
-suspend inline fun <R> ViewModel.executeUseCase(block: () -> R): Result<R> {
-    return viewModelScope.executeUseCase(block)
-}
